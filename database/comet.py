ca20daf5dfed0b0eec77cfa0907688dee12cd3de
chung-ejy <chung-ejy@gmail.com>
2022-01-24 03:27:15 -0500

    removed trailing merge comments

--- database/comet.py
+++ database/comet.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-from database.adatabase import ADatabase
-import pandas as pd
-class Comet(ADatabase):
-    
-    def __init__(self):
-        super().__init__("comet")
-    
-
-    def retrieve_fills(self):
-        try:
-            db = self.client[self.name]
-            table = db["cloud_fills"]
-            data = table.find({},{"order_id":1,"_id":0},show_record_id=False)
-            return pd.DataFrame(list(data))
-        except Exception as e:
-            print(self.name,"fills",str(e))
-    
-=======
 from database.adatabase import ADatabase
 import pandas as pd
 class Comet(ADatabase):
@@ -69,4 +50,3 @@ class Comet(ADatabase):
             return pd.DataFrame(list(data))
         except Exception as e:
             print(self.name,"fills",str(e))
->>>>>>> cloud_test
