ca9f289dfe099b8a25d283f977d5afb76720f816
dev-ejc <chung.ejy@gmail.com>
2022-01-30 23:07:54 -0500

    added xapikey header

--- comet_historian/comet_historian.py
+++ comet_historian/comet_historian.py
@@ -10,7 +10,8 @@ class CometHistorian(object):
     def entry_analysis(self,entry_strategy,merged,signal,value,conservative,req):
         base_url = "https://cometchaserapi.herokuapp.com/api/analysis/"
         headers = {
-            "Content-type":"application/json"
+            "Content-type":"application/json",
+            "X-Api-Key":key
         }
         stuff = {"entry_strategy":entry_strategy,"data":merged.to_dict("records")
         ,"signal":signal,"value":value,"conservative":conservative,
@@ -23,7 +24,8 @@ class CometHistorian(object):
     def exit_analysis(self,exit_strategy,order,merged,req):
         base_url = "https://cometcometchaser.herokuapp.com/api/analysis/"
         headers = {
-            "Content-type":"application/json"
+            "Content-type":"application/json",
+            "X-Api-Key":key
         }
         stuff = {"exit_strategy":exit_strategy,"order":order,"data":merged.to_dict("records"),"req":req,"key":key,"side":"exit"}
         response = r.post(base_url,headers=headers,params=json.dumps(stuff).encode("utf-8"))
