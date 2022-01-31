20d5f3729b04afe5484124bc8797be17f72e4f3c
dev-ejc <chung.ejy@gmail.com>
2022-01-31 06:16:16 -0500

    ignoring fees for now

--- main.py
+++ main.py
@@ -231,7 +231,6 @@ while live:
                     complete_trade_ids = complete_trades["order_id"].unique()
                 if pending_trades.index.size > 0:
                     complete_trades = pending_trades[(~pending_trades["order_id"].isin(complete_trade_ids)) & (pending_trades["order_id"].isin(complete_buy_ids)) & (pending_trades["sell_id"].isin(complete_sell_ids))]
-                    complete_trades["fee"] = [float(x) for x in complete_trades["fee"]]
                     ct = complete_trades.groupby(["product_id","order_id"]).agg({"sell_price":"mean","size":"sum","price":"mean","date":"first"}).reset_index()
                     if ct.index.size > 0:
                         ct["username"] = user
